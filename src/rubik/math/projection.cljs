bfee42073cabd69e284aa8dac997170c12025080
Tobias Grelsson <tgrelsson@gmail.com>
2024-01-16 00:56:08 +0100

    inverse stereographic projection

--- src/rubik/math/projection.cljs
+++ src/rubik/math/projection.cljs
@@ -5,6 +5,13 @@
    [scale (fn [a] (/ a (- 1 z)))]
     #js [(scale x) (scale y) 0]))
 
+(defn inverse-stereographic [[x y]]
+  (let
+   [square (+ (* x x) (* y y))
+    z (/ (dec square) (inc square))
+    scale (partial * (- 1 z))]
+    [(scale x) (scale y) z]))
+
 (defn orthographic [a]
   (let
    [[x y _] a]
